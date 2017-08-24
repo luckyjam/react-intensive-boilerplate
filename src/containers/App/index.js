96f73ccf36a5e5b85bfbb1a4711cdf3a14b0312d
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-08-24 14:17:54 +0300

    finish essay

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -6,12 +6,14 @@ import Styles from './styles.scss';
 
 // Components
 import Composer from '../../components/Composer';
+import Post from '../../components/Post';
 
 export default class App extends Component {
     render () {
         return (
             <section className = { Styles.app }>
                 <Composer />
+                <Post />
             </section>
         );
     }
