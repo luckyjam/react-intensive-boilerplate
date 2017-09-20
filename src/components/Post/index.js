cd7b10315541603c38468fbc49cef94e7a0a5fa1
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-09-20 22:49:37 +0300

    quick update

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,18 +3,16 @@ import React, { Component } from 'react';
 
 // Instruments
 import homer from '../../theme/assets/homer.png';
-import { getCurrentTime } from '../../helpers';
+import moment from 'moment';
 
 export default class Post extends Component {
     render () {
         return (
             <section>
                 <img alt = 'commenter' src = { homer } />
-                <span>Homer Simpson</span>
-                <p>
-                    It is {getCurrentTime()}.
-                </p>
-                <span>It is donuts time! Doh!</span>
+                <a>Homer Simpson</a>
+                <a>It is {moment().format('MMMM D h:mm:ss a')}.</a>
+                <p>It is donuts time! Doh!</p>
             </section>
         );
     }
